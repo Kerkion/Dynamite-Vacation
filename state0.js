0165590a4a28ff4633667a32fcf0f9a53bf9e30c
Kerkion <kerkion1984@gmail.com>
2018-07-15 20:29:17 +0200

    added align to center the game,added scaling for game

--- state0.js
+++ state0.js
@@ -1,7 +1,12 @@
 var demo = {};
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function(){},
-    create: function(){},
+    preload: function(){
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    },
+    create: function(){
+    },
     update: function(){}
 };
--- state1.js
+++ state1.js
@@ -1,7 +1,8 @@
-var demo = {};
 demo.state1 = function(){};
 demo.state1.prototype = {
     preload: function(){},
-    create: function(){},
+    create: function(){
+       
+    },
     update: function(){}
 };
