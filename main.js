2739507268cbb25286817c8e6c6e589f50631e9a
Kerkion <kerkion1984@gmail.com>
2018-07-16 12:17:29 +0200

    added background and rest of the sprites,centered anchor to middle

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(640,960,Phaser.AUTO);
+var game = new Phaser.Game(640,960,Phaser.CANVAS);
 game.state.add('state0',demo.state0);
 game.state.add('state1',demo.state1);
 game.state.start('state0');
--- state0.js
+++ state0.js
@@ -1,12 +1,24 @@
-var demo = {};
+var demo = {},centerX = 640 / 2,centerY = 960/2;
 demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function(){
+        game.load.image('Bot','assets/sprites/Bot.png');
+        game.load.image('BG','assets/sprites/Background.png');
+        game.load.image('Player','assets/sprites/Player.png');
+        game.load.image('Bomb','assets/sprites/bomb small.png');
+    },
+    create: function(){
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
-    },
-    create: function(){
+
+    game.add.sprite(0,0,'BG');
+    var bot = game.add.sprite(centerX,300,'Bot');
+    bot.anchor.setTo(0.5,0.5);
+    var player = game.add.sprite(centerX,890,'Player');
+    player.anchor.setTo(0.5,0.5);
+    var bomb = game.add.sprite(centerX,centerY,'Bomb');
+    bomb.anchor.setTo(0.5,0.5);
     },
     update: function(){}
 };
